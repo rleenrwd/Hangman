1d3d4ea2d0da87742f32f18e13bdc4dd8c0980c8
Norwood91 <robnorwood91@gmail.com>
2025-05-18 23:58:52 -0700

    Finished the game, for now. Added some user friendly print statements to show how many lives they have during gameplay.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-import os 
+
 
 print("Welcome to the classic game of Hangman!")
 print("Try to guess the below word")
@@ -13,10 +13,6 @@ placeholder = ""
 
 word_length = len(word_to_guess)
 
-lives = 6
-
-print(word_to_guess)
-
 # Display underscores in replace of the letters in the word
 for position in range(word_length):
     placeholder += "_"
@@ -27,13 +23,17 @@ game_over = False
 
 correct_letters = []
 
+lives = 6
+print(f"Your starting lives are: {lives}")
+
 while not game_over:
     
     # Let the user guess
     user_guess = input("What letter do you think is in the word?: ").strip().lower()
+    
+    
     word_to_display = ""
-   
-
+    
     # Replace the underscore with the correct letter if it's in the word
     for letter in word_to_guess:
         if letter == user_guess:
@@ -44,12 +44,35 @@ while not game_over:
         else:
            word_to_display += "_"
            
-           
-    
     print(word_to_display)
     
-           
+
+    # Handle decrementing the user's lives if they get the answer wrong
+    if user_guess not in word_to_guess:
+        print(f"'{user_guess}' is not in the word, try again.")
+        lives -= 1
+        print(f"\nLives left: {lives}")
+
+        
+        if lives == 0:
+            print(word_to_display)
+            print(f"The word to guess was: {word_to_guess}")
+            print()
+            print("GAME OVER. YOU RAN OUT OF LIVES!")
+            game_over = True
+    else:
+        print(f"'{user_guess}' is in the word! Keep going!")
+        print(f"\nLives left: {lives}")
+        
+
+    # Handle if the user wins the game
     if "_" not in word_to_display:
-        print("YOU WIN!")
+        print("YOU WIN! GREAT JOB!")
         game_over = True
+
+
+    
+
+    
+    
 
