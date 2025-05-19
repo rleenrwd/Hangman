6658b602aab1456d630a1fbe4e673d96f6e84efd
Norwood91 <robnorwood91@gmail.com>
2025-05-18 20:13:45 -0700

    Added some of the basic functions of the game. Will add to this.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("This is the start of the project!")
+import random
+
+print('Welcome to the classic game of Hangman!')
+
+word_list = ['hello', 'goodbye', 'random', 'jello', 'monster', 'camel']
+
+# Randomly choose a word from the word list
+word_to_guess = random.choice(word_list)
+
+# Let the user guess
+user_guess = input("Try to guess a letter in the word!: ").strip().lower()
+
+if user_guess in word_to_guess:
+    print("Right!")
+else:
+    print("Wrong")
+
