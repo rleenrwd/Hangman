894fe94a6e4921a0cb34f57955ba19a8193684ac
Norwood91 <robnorwood91@gmail.com>
2025-05-18 22:15:15 -0700

    Refactored the program. Added variables to hold the lives, and word_length. Refactored the placeholder code.

--- main.py
+++ main.py
@@ -8,26 +8,39 @@ word_list = ['hello', 'goodbye', 'random', 'jello', 'monster', 'camel']
 
 # Randomly choose a word from the word list
 word_to_guess = random.choice(word_list)
+
 placeholder = ""
-word_to_display = ""
+
+word_length = len(word_to_guess)
+
+lives = 6
+
 print(word_to_guess)
 
 # Display underscores in replace of the letters in the word
-for letter in word_to_guess:
-    letter = "_"
-    placeholder += letter
+for position in range(word_length):
+    placeholder += "_"
 
 print(placeholder, "\n")
 
-# Let the user guess
-user_guess = input("What letter do you think is in the word?: ").strip().lower()
+game_over = False
 
-# Replace the underscore with the correct letter if it's in the word
-for letter in word_to_guess:
-    if user_guess != letter:
-        letter = "_"
-        word_to_display += letter
-    else:
-        letter = user_guess
-        word_to_display += letter
-print(word_to_display)
+while not game_over:
+    # Let the user guess
+    user_guess = input("What letter do you think is in the word?: ").strip().lower()
+
+    word_to_display = ""
+
+    # Replace the underscore with the correct letter if it's in the word
+    for letter in word_to_guess:
+        if letter == user_guess:
+            word_to_display += letter
+        else:
+           word_to_display += "_"
+
+    print(word_to_display)
+           
+    if "_" not in word_to_display:
+        print("YOU WIN!")
+        game_over = True
+
